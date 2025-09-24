4bfc23b6b498fdd9802f07cf232dd12d79d8a095
Nguyen Minh Tuan Khanh <khanhblue06@gmail.com>
2025-09-24 19:39:39 +0700

    Khởi tạo project Java và kết nối với repo

---
+++ src/main/java/edu/uth/online_course_platform/OnlineCoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.online_course_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCoursePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uth/online_course_platform/OnlineCoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.online_course_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
